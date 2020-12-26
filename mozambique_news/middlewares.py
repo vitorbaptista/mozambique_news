2fe1fefd6598be0a21047c8d7e01c86b466339d0
Vitor Baptista <vitor@vitorbaptista.com>
2020-12-26 12:00:40 -0300

    Run Black

--- mozambique_news/middlewares.py
+++ mozambique_news/middlewares.py
@@ -53,7 +53,7 @@ class MozambiqueNewsSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class MozambiqueNewsDownloaderMiddleware:
@@ -100,4 +100,4 @@ class MozambiqueNewsDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
--- mozambique_news/spiders/jornalnoticias_co_mz.py
+++ mozambique_news/spiders/jornalnoticias_co_mz.py
@@ -3,14 +3,14 @@ import scrapy
 
 
 class JornalnoticiasCoMzSpider(scrapy.Spider):
-    name = 'jornalnoticias_co_mz'
-    allowed_domains = ['www.jornalnoticias.co.mz']
+    name = "jornalnoticias_co_mz"
+    allowed_domains = ["www.jornalnoticias.co.mz"]
     start_urls = [
-        'https://www.jornalnoticias.co.mz/index.php/politica',
-        'https://www.jornalnoticias.co.mz/index.php/sociedade',
-        'https://www.jornalnoticias.co.mz/index.php/capital/maputo',
-        'https://www.jornalnoticias.co.mz/index.php/capital/beira',
-        'https://www.jornalnoticias.co.mz/index.php/capital/nampula',
+        "https://www.jornalnoticias.co.mz/index.php/politica",
+        "https://www.jornalnoticias.co.mz/index.php/sociedade",
+        "https://www.jornalnoticias.co.mz/index.php/capital/maputo",
+        "https://www.jornalnoticias.co.mz/index.php/capital/beira",
+        "https://www.jornalnoticias.co.mz/index.php/capital/nampula",
     ]
 
     def parse(self, response):
@@ -19,48 +19,52 @@ class JornalnoticiasCoMzSpider(scrapy.Spider):
             yield news_data
 
         try:
-            start_date = datetime.datetime.strptime(self.start_date, '%Y-%m-%d')
-            if news_data['published_at'].replace(tzinfo=None) < start_date.replace(tzinfo=None):
+            start_date = datetime.datetime.strptime(self.start_date, "%Y-%m-%d")
+            if news_data["published_at"].replace(tzinfo=None) < start_date.replace(
+                tzinfo=None
+            ):
                 return  # Stop scraping
         except AttributeError:
             pass
 
-        next_page = response.css('.pagination .seguinte a::attr(href)').get()
+        next_page = response.css(".pagination .seguinte a::attr(href)").get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_news(self, selector):
         extractors = {
-            'published_at': '[itemprop="datePublished"]::attr(datetime)',
-            'title': '[itemprop="name"] a::text',
-            'url': '[itemprop="name"] a::attr(href)',
-            'category': '[itemprop="genre"]::text',
-            'views': '[itemprop="interactionCount"]::attr(content)',
-            'body': 'span::text',
+            "published_at": '[itemprop="datePublished"]::attr(datetime)',
+            "title": '[itemprop="name"] a::text',
+            "url": '[itemprop="name"] a::attr(href)',
+            "category": '[itemprop="genre"]::text',
+            "views": '[itemprop="interactionCount"]::attr(content)',
+            "body": "span::text",
         }
 
         data = {
-            key: '\n'.join([row.strip() for row in selector.css(extractor).extract()])
+            key: "\n".join([row.strip() for row in selector.css(extractor).extract()])
             for (key, extractor) in extractors.items()
         }
-        data['url'] = f"https://www.jornalnoticias.co.mz{data['url']}"
-        data['views'] = data['views'].split(':')[-1]
+        data["url"] = f"https://www.jornalnoticias.co.mz{data['url']}"
+        data["views"] = data["views"].split(":")[-1]
 
-        if not data['body']:
-            data['body'] = '\n'.join([row.strip() for row in selector.css('p::text').extract()])
+        if not data["body"]:
+            data["body"] = "\n".join(
+                [row.strip() for row in selector.css("p::text").extract()]
+            )
 
-        if not data['body']:
-            all_text = [row.strip() for row in selector.css('::text').extract()]
-            text_start_index = all_text.index('Twitter')
-            text_end_index = all_text.index('Comments')
+        if not data["body"]:
+            all_text = [row.strip() for row in selector.css("::text").extract()]
+            text_start_index = all_text.index("Twitter")
+            text_end_index = all_text.index("Comments")
 
-            data['body'] = '\n'.join(all_text[text_start_index + 1:text_end_index])
+            data["body"] = "\n".join(all_text[text_start_index + 1 : text_end_index])
 
         for key in extractors.keys():
             assert data[key], f"Key {key} is empty: {data}"
 
-        data['published_at'] = datetime.datetime.strptime(data['published_at'], '%Y-%m-%dT%H:%M:%S%z')
+        data["published_at"] = datetime.datetime.strptime(
+            data["published_at"], "%Y-%m-%dT%H:%M:%S%z"
+        )
 
         return data
-
-
